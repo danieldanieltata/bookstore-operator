d187d073559cddd8595c25dcce9fbdd5cd3fd652
Daniel Talor Moses <danieltalor1@gmail.com>
2026-02-07 20:00:25 +0200

    Handle deletions by adding finalizer
    Signed-off-by: Daniel Talor Moses <danieltalor1@gmail.com>

--- internal/controller/bookstore_controller.go
+++ internal/controller/bookstore_controller.go
@@ -24,6 +24,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	bookstoreexamplecomv1 "github.com/danieldanieltata/bookstore-operator/api/v1"
@@ -31,6 +32,8 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+const bookStoreFinalizer = "bookstore.example.com/finalizer"
 
 // BookStoreReconciler reconciles a BookStore object
 type BookStoreReconciler struct {
@@ -41,6 +44,7 @@ type BookStoreReconciler struct {
 // +kubebuilder:rbac:groups=bookstore.example.com,resources=bookstores,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=bookstore.example.com,resources=bookstores/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=bookstore.example.com,resources=bookstores/finalizers,verbs=update
+// +kubebuilder:rbac:groups=bookstore.example.com,resources=books,verbs=list;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -56,7 +60,43 @@ func (r *BookStoreReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 
 	log.Info("Reconciling BookStore", "request", req)
 
-	// Create namespace with the name of the BookStore if not exists
+	// Fetch the BookStore, if not found, it was deleted, do not create or update anything.
+	bookstore := &bookstoreexamplecomv1.BookStore{}
+	err := r.Get(ctx, req.NamespacedName, bookstore)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	// Handle deletion: delete all related Books then remove finalizer.
+	if bookstore.DeletionTimestamp != nil {
+		log.Info("BookStore is being deleted, running finalizer cleanup", "bookstore", req.NamespacedName, "namespace", bookstore.Namespace)
+		if controllerutil.ContainsFinalizer(bookstore, bookStoreFinalizer) {
+			if err := r.deleteBooksForBookStore(ctx, bookstore); err != nil {
+				return ctrl.Result{}, err
+			}
+			controllerutil.RemoveFinalizer(bookstore, bookStoreFinalizer)
+			if err := r.Update(ctx, bookstore); err != nil {
+				log.Error(err, "Failed to remove finalizer", "bookstore", req.NamespacedName)
+				return ctrl.Result{}, err
+			}
+			log.Info("Finalizer removed, BookStore will be deleted", "bookstore", req.NamespacedName)
+		}
+		return ctrl.Result{}, nil
+	}
+
+	// Ensure finalizer is set so we can clean up on delete.
+	if !controllerutil.ContainsFinalizer(bookstore, bookStoreFinalizer) {
+		controllerutil.AddFinalizer(bookstore, bookStoreFinalizer)
+		if err := r.Update(ctx, bookstore); err != nil {
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{Requeue: true}, nil
+	}
+
+	// Create namespace with the name of the BookStore if it does not exist.
 	namespace := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: req.Name,
@@ -64,20 +104,67 @@ func (r *BookStoreReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 	}
 
 	foundNamespace := &corev1.Namespace{}
-	err := r.Get(ctx, types.NamespacedName{Name: req.Name}, foundNamespace)
+	err = r.Get(ctx, types.NamespacedName{Name: req.Name}, foundNamespace)
 	if err != nil && errors.IsNotFound(err) {
-		err = r.Create(ctx, namespace)
-
-		if err != nil {
+		if err = r.Create(ctx, namespace); err != nil {
 			return ctrl.Result{}, err
 		}
 
+		bookstore.SetOwnerReferences([]metav1.OwnerReference{
+			{
+				APIVersion: "v1",
+				Kind:       "Namespace",
+				Name:       namespace.Name,
+				UID:        namespace.UID,
+			},
+		})
+		err = r.Update(ctx, bookstore)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
 		log.Info("Namespace created", "namespace", namespace.Name)
 	} else if err != nil {
 		return ctrl.Result{}, err
 	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *BookStoreReconciler) deleteBooksForBookStore(ctx context.Context, bookstore *bookstoreexamplecomv1.BookStore) error {
+	log := logf.FromContext(ctx)
+
+	// bookstore namespace is the same as the bookstore name, i wasn't sure if we wanted to put the bookstore in the created namespace or not.
+	bookstoreNS := bookstore.ObjectMeta.Name
+
+	var inNSList bookstoreexamplecomv1.BookList
+	if err := r.List(ctx, &inNSList, client.InNamespace(bookstoreNS)); err != nil {
+		return err
+	}
+	for i := range inNSList.Items {
+		b := &inNSList.Items[i]
+		if err := r.Delete(ctx, b); err != nil && !errors.IsNotFound(err) {
+			return err
+		}
+		log.Info("Deleted Book in bookstore namespace", "book", b.Name, "namespace", b.Namespace)
+	}
+
+	var allBooks bookstoreexamplecomv1.BookList
+	if err := r.List(ctx, &allBooks); err != nil {
+		return err
+	}
+	for i := range allBooks.Items {
+		b := &allBooks.Items[i]
+
+		if b.Spec.CopyOf == nil || b.Spec.CopyOf.Namespace != bookstoreNS {
+			continue
+		}
+		if err := r.Delete(ctx, b); err != nil && !errors.IsNotFound(err) {
+			return err
+		}
+		log.Info("Deleted copy Book", "book", b.Name, "namespace", b.Namespace, "copyOf", b.Spec.CopyOf)
+	}
+
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
